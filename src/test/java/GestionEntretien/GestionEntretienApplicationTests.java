ca5a758bd174e82582345d3efa98737b28cac457
Zakaria <Zakaria@hp>
2020-04-17 19:54:07 +0100

    First Push

---
+++ src/test/java/GestionEntretien/GestionEntretienApplicationTests.java
@@ -0,0 +1,13 @@
+package GestionEntretien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEntretienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
