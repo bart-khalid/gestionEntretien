661b185da6db121db689557fded24bc285719257
zakaria <zikochakir@gmail.com>
2020-05-21 00:46:00 +0000

    done

--- src/main/java/GestionEntretien/ServiceImpl/VehiculeImpl.java
+++ src/main/java/GestionEntretien/ServiceImpl/VehiculeImpl.java
@@ -5,7 +5,13 @@
  */
 package GestionEntretien.ServiceImpl;
 
+import GestionEntretien.Bean.BonCarburant;
+import GestionEntretien.Bean.BonReparation;
+import GestionEntretien.Bean.BonVidange;
 import GestionEntretien.Bean.Vehicule;
+import GestionEntretien.Dao.BonCarburantRepository;
+import GestionEntretien.Dao.BonReparationRepository;
+import GestionEntretien.Dao.BonVidangeRepository;
 import GestionEntretien.Dao.VehiculeRepository;
 import GestionEntretien.Service.VehiculeService;
 import java.util.List;
@@ -18,18 +24,24 @@ import org.springframework.stereotype.Service;
  * @author lenovo
  */
 @Service
-public class VehiculeImpl implements VehiculeService{
+public class VehiculeImpl implements VehiculeService {
 
     @Autowired
     private VehiculeRepository vehiculeRepository;
-    
+    @Autowired
+    private BonCarburantRepository boncarrepo;
+    @Autowired
+    private BonReparationRepository bonreparepo;
+    @Autowired
+    private BonVidangeRepository bonvidrepo;
+
     @Override
     public int save(Vehicule vehicule) {
         Vehicule foundedv = vehiculeRepository.findByMatricule(vehicule.getMatricule());
-        if(foundedv!=null){
-        return -1;
+        if (foundedv != null) {
+            return -1;
         }
-        vehicule.setDescriptionDropDown(vehicule.getMatricule()+','+vehicule.getMarque());
+        vehicule.setDescriptionDropDown(vehicule.getMatricule() + ',' + vehicule.getMarque());
         Vehicule.setNbr(vehicule.getNbr() + 1);
         vehicule.setReference(RandomStringUtils.random(6, true, false) + String.valueOf(vehicule.getNbr()));
         vehiculeRepository.save(vehicule);
@@ -38,20 +50,41 @@ public class VehiculeImpl implements VehiculeService{
 
     @Override
     public int update(Vehicule vehicule) {
-    Vehicule foundedv = vehiculeRepository.findByReference(vehicule.getReference());
-    foundedv.setDescriptionDropDown(vehicule.getMatricule()+','+vehicule.getMarque());
-    foundedv.setMatricule(vehicule.getMatricule());
-    foundedv.setMarque(vehicule.getMarque());
-    foundedv.setType(vehicule.getType());
-    foundedv.setUtilite(vehicule.getUtilite());
-    foundedv.setDateEntrerParc(vehicule.getDateEntrerParc());
-    vehiculeRepository.save(foundedv);
-    return 1;
+        Vehicule foundedv = vehiculeRepository.findByReference(vehicule.getReference());
+        foundedv.setDescriptionDropDown(vehicule.getMatricule() + ',' + vehicule.getMarque());
+        foundedv.setMatricule(vehicule.getMatricule());
+        foundedv.setMarque(vehicule.getMarque());
+        foundedv.setType(vehicule.getType());
+        foundedv.setUtilite(vehicule.getUtilite());
+        foundedv.setDateEntrerParc(vehicule.getDateEntrerParc());
+        List<BonCarburant> listBons = boncarrepo.findAll();
+        List<BonReparation> listBonsrepa = bonreparepo.findAll();
+        List<BonVidange> listBonsvid = bonvidrepo.findAll();
+        for (BonCarburant bon : listBons) {
+            if (bon.getVehiculeC().getReference() == foundedv.getReference()) {
+                bon.setVehiculeassooci(foundedv.getDescriptionDropDown());
+            }
+            boncarrepo.save(bon);
+        }
+        for (BonReparation bon : listBonsrepa) {
+            if (bon.getVehiculeR().getReference() == foundedv.getReference()) {
+                bon.setVehiculeassooci(foundedv.getDescriptionDropDown());
+            }
+            bonreparepo.save(bon);
+        }
+        for (BonVidange bon : listBonsvid) {
+            if (bon.getVehiculeV().getReference() == foundedv.getReference()) {
+                bon.setVehiculeassooci(foundedv.getDescriptionDropDown());
+            }
+            bonvidrepo.save(bon);
+        }
+        vehiculeRepository.save(foundedv);
+        return 1;
     }
 
     @Override
     public int delete(String reference) {
-    Vehicule foundedv = vehiculeRepository.findByReference(reference);
+        Vehicule foundedv = vehiculeRepository.findByReference(reference);
         vehiculeRepository.delete(foundedv);
         return 1;
     }
@@ -63,12 +96,12 @@ public class VehiculeImpl implements VehiculeService{
 
     @Override
     public Vehicule findByReference(String reference) {
-        return vehiculeRepository.findByReference(reference);  
+        return vehiculeRepository.findByReference(reference);
     }
 
     @Override
     public Vehicule findByMatricule(String Matricule) {
-        return vehiculeRepository.findByMatricule(Matricule); 
+        return vehiculeRepository.findByMatricule(Matricule);
     }
-    
+
 }
